437baa202d2f4e329c3566d34ee70e2014fe8f7b
fedeoo <xt_devil@sina.com>
2017-03-19 21:27:03 +1100

    fix

--- mobile_adaptor_proposel.js
+++ mobile_adaptor_proposel.js
@@ -19,7 +19,7 @@
 
 
         // 设置viewport，进行缩放，达到高清效果
-        metaEl.setAttribute('content', 'width=' + dpr * docEl.clientWidth + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
+        metaEl.setAttribute('content', 'width=' + dpr * window.screen.width + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
 
         // 设置data-dpr属性，留作的css hack之用
         docEl.setAttribute('data-dpr', dpr);
